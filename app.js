5ca3c09668768060f0eb7dc207ffa2c7672cb954
Elkin Narvaez <elkinnarvaez2406@gmail.com>
2022-09-26 01:05:41 -0500

    install dependencies

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get(['/', '/:name'], (req, res) => {
   greeting = '<h1>Hello From Node on Fly!</h1>';
