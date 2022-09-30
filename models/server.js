41e8d66a8b8ea4ea70c28b8866623fb9322562ed
Elkin Narvaez <elkinnarvaez2406@gmail.com>
2022-09-30 03:01:04 -0500

    Add env file

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || 8080;
+    this.port = process.env.PORT;
     this.paths = {
       homepage: '/api/homepage',
     };
